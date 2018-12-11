a4ace876e3f7c78532516f9240ba0c11a741efce
pynnl <pynnl@outlook.com>
2018-12-11 17:44:39 -0500

    Add tests for lenEncoder

--- tests/maxEncoder.test.js
+++ tests/maxEncoder.test.js
@@ -2,24 +2,24 @@ const Hashnum = require('../lib')
 
 describe('Encoding returns empty string on invalid input:', () => {
   it('Null', () => {
-    expect(Hashnum.lenEncode(null)).toEqual('')
-    expect(Hashnum.lenEncode()).toEqual('')
+    expect(Hashnum.maxEncode(null)).toEqual('')
+    expect(Hashnum.maxEncode()).toEqual('')
   })
 
   it('Number', () => {
-    [0, 1, 99].forEach(e => expect(Hashnum.lenEncode(e)).toEqual(''))
+    [0, 1, 99].forEach(e => expect(Hashnum.maxEncode(e)).toEqual(''))
   })
 
   it('String', () => {
-    ['', 'a', '99'].forEach(e => expect(Hashnum.lenEncode(e)).toEqual(''))
+    ['', 'a', '99'].forEach(e => expect(Hashnum.maxEncode(e)).toEqual(''))
   })
 
   it('Object', () => {
-    expect(Hashnum.lenEncode({})).toEqual('')
+    expect(Hashnum.maxEncode({})).toEqual('')
   })
 
   it('Negative number in array', () => {
-    expect(Hashnum.lenEncode([-1])).toEqual('')
+    expect(Hashnum.maxEncode([-1])).toEqual('')
   })
 })
 
@@ -49,28 +49,28 @@ describe('Decoding returns empty array on invalid input:', () => {
 describe('Encoding and decoding are matched on array:', () => {
   it('Of number', () => {
     const data = [1, 23, 456, 7890]
-    const str = Hashnum.lenEncode(data)
+    const str = Hashnum.maxEncode(data)
     const nums = Hashnum.decode(str)
     expect(nums).toEqual(data)
   })
 
   it('Of valid string', () => {
     const data = ['1', '23', '456', '7890']
-    const str = Hashnum.lenEncode(data)
+    const str = Hashnum.maxEncode(data)
     const nums = Hashnum.decode(str)
     expect(nums).toEqual(data.map(e => Number(e)))
   })
 
   it('Of mix string and number', () => {
     const data = ['0', 0, '0', 1]
-    const str = Hashnum.lenEncode(data)
+    const str = Hashnum.maxEncode(data)
     const nums = Hashnum.decode(str)
     expect(nums).toEqual(data.map(e => Number(e)))
   })
 
   it('Start with 0', () => {
     const data = [0, 0, 0, 1]
-    const str = Hashnum.lenEncode(data)
+    const str = Hashnum.maxEncode(data)
     const nums = Hashnum.decode(str)
     expect(nums).toEqual(data)
   })
@@ -78,38 +78,29 @@ describe('Encoding and decoding are matched on array:', () => {
 
 describe('Encoding pattern', () => {
   it('Normal', () => {
-    const str = Hashnum.lenEncode([1, 2, 3])
-    const occur = (str.match(/i/g) || []).length
+    const str = Hashnum.maxEncode([1, 2, 3])
+    const occur = (str.match(/e/g) || []).length
+    expect(occur).toEqual(0)
+  })
+
+  it('Start with 0', () => {
+    const str = Hashnum.maxEncode([0, 1, 2, 3])
+    const occur = (str.match(/e/g) || []).length
+    expect(str[0]).toEqual('e')
     expect(occur).toEqual(1)
   })
 
-  it('>=2 numbers < table len', () => {
-    const str = Hashnum.lenEncode([1, 1, 2])
-    const occur = (str.match(/i/g) || []).length
-    expect(occur).toEqual(2)
-  })
-
-  it('same 0s vs same numbers < table len', () => {
-    const str = Hashnum.lenEncode([0, 0, 1, 1])
-    const occur = (str.match(/i/g) || []).length
+  it('Max number >= table len', () => {
+    const str = Hashnum.maxEncode([1, 2, 99])
+    const occur = (str.match(/e/g) || []).length
+    expect(str[0]).not.toEqual('e')
     expect(occur).toEqual(1)
   })
 
-  it('1 number > table len', () => {
-    const str = Hashnum.lenEncode([1, 2, 99])
-    const occur = (str.match(/i/g) || []).length
-    expect(occur).toEqual(1)
-  })
-
-  it('2 numbers > table len', () => {
-    const str = Hashnum.lenEncode([1, 99, 99])
-    const occur = (str.match(/i/g) || []).length
-    expect(occur).toEqual(2)
-  })
-
-  it('same 0s vs same very big numbers', () => {
-    const str = Hashnum.lenEncode([0, 0, 999, 999])
-    const occur = (str.match(/i/g) || []).length
+  it('Start with 0 & Max number >= table len', () => {
+    const str = Hashnum.maxEncode([0, 1, 2, 99])
+    const occur = (str.match(/e/g) || []).length
+    expect(str[0]).toEqual('e')
     expect(occur).toEqual(2)
   })
 })
