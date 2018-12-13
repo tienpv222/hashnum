d1ac3a2b7488e7aa49cd8c503e1314ab7fb2439b
pynnl <pynnl@outlook.com>
2018-12-13 01:14:53 -0500

    Create basic doc

---
+++ docs-src/App.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+#app(spellcheck='false')
+  #header
+    div header
+  #body
+    Encoder
+    Decoder
+  #footer
+    div body
+</template>
+
+<script>
+import Encoder from './components/Encoder'
+import Decoder from './components/Decoder'
+
+export default {
+  components: {
+    Encoder,
+    Decoder,
+  },
+}
+</script>
+
+<style lang="stylus">
+@require 'styles/colors'
+@require 'styles/app'
+@require 'styles/body'
+@require 'styles/input'
+
+</style>
---
+++ docs-src/components/Encoder.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+#encoder.input
+  span [
+  span
+    template(v-for='(e,i) in nums')
+      Number(
+        :ref='"num" + i'
+        :nums='nums'
+        :i='i'
+        @nums-change='onNumsChange'
+        @focus-change='onFocusChange')
+  span ]
+</template>
+
+<script>
+import Number from './Number'
+
+export default {
+  components: { Number },
+  data: vm => ({
+    nums: ['123', '6789', 'abcde'],
+  }),
+  mounted(vm = this) {
+  },
+  methods: {
+    onNumsChange(start, end, val) {
+      this.nums.splice(start, end, val)
+    },
+    onFocusChange(i, caret, vm = this) {
+      vm.$nextTick(() => vm.$refs['num' + i][0].setFocus(caret))
+    },
+
+  },
+}
+</script>
